f0b13d6c8ca1ad4bffdf41439fe525fd7a76cc62
Renewwen <renewwenz@gmail.com>
2020-05-17 21:50:48 -0700

    IoC Dependency lookup overview

---
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/renewwen/thinking/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -0,0 +1,65 @@
+package org.renewwen.thinking.in.spring.ioc.overview.dependency.lookup;
+
+import java.util.Map;
+
+import org.renewwen.thinking.in.spring.ioc.overview.annotation.Super;
+import org.renewwen.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 依赖查找实例
+ * 1. 通过名称的方式来查找
+ */
+public class DependencyLookupDemo {
+
+    public static void main(String[] args) {
+        // 配置 XML 配置文件
+        // 启动 Spring 应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-lookup-context.xml");
+        // 类型：查找对象
+        lookupByType(beanFactory);
+        // 类型：查找结合对象
+        lookupCollectionByType(beanFactory);
+        // 注解：查找对象
+        lookupByAnnotationType(beanFactory);
+        // 名称：延迟查找
+        lookupInRealTime(beanFactory);
+        // 名称：实时查找
+        lookupInLazy(beanFactory);
+    }
+
+    private static void lookupByAnnotationType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = (Map) listableBeanFactory.getBeansWithAnnotation(Super.class);
+            System.out.println("查找标注 @Super 所有的 User 集合对象: " + users);
+        }
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("查找到的所有 User 集合对象: " + users);
+        }
+    }
+
+    private static void lookupByType(BeanFactory beanFactory) {
+        User user = beanFactory.getBean(User.class);
+        System.out.println("实时查找: " + user);
+    }
+
+    private static void lookupInLazy(BeanFactory beanFactory) {
+        ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+        User user = objectFactory.getObject();
+        System.out.println("延迟查找: " + user);
+    }
+
+    private static void lookupInRealTime(BeanFactory beanFactory) {
+        User user = (User) beanFactory.getBean("user");
+        System.out.println("实时查找: " + user);
+    }
+}
